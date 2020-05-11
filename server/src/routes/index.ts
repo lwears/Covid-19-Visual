195f46ccca4902a4c62f0fc8ab3e6e66cee74559
Liam Wears <liamwears@fastmail.com>
2020-05-11 21:44:42 +0200

    trying to deploy to heroku

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,8 +5,12 @@ const router = express.Router();
 
 /* GET React Frontend. */
 router.get('/', (_req, res) => {
-  console.log(`${path.resolve('../')}client/build/index.html`);
-  res.sendFile(`${path.resolve('../')}client/build/index.html`);
+  console.log(
+    `${path.resolve('../')}/covid-19-web-app/client/build/index.html`
+  );
+  res.sendFile(
+    `${path.resolve('../')}/covid-19-web-app/client/build/index.html`
+  );
 });
 
 export default router;
